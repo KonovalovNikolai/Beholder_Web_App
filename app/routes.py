8572f316aecd6100d0f18b47b8ac906495561434
Nikolai Konovalov <konovv.nikk@gmail.com>
2021-03-25 16:45:41 +0300

    Вход на сайт

--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,40 @@
 from flask import render_template, flash, redirect, request, url_for
-from app import app
+from flask_login import current_user, login_user, logout_user
 
+from app import app
 from app.forms import LoginForm
+from app.models import User
 
 
 @app.route('/')
 def index():
     return render_template('index.html', title='Home')
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
+    # Если пользователь входил ранее
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
 
+    form = LoginForm()
+    
+    # Проверка корректности данных, если метод POST
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(form.username.data, 
-            form.remember_me.data))
+        user = User.query.filter_by(email=form.email.data).first()
+
+        # Проверка введённых
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     
     return render_template('login.html', title='Sing In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
