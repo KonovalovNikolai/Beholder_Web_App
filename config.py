702158653dc46eb90e44ac91c94e2a016c5a952a
Nikolai Konovalov <konovv.nikk@gmail.com>
2021-05-10 14:41:11 +0300

    замена бд

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "28e29edcfbb94deca66793fdbd0ec36b"
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://beholder:123789qzectb@localhost/beholder'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SEND_FILE_MAX_AGE_DEFAULT = 0
 
